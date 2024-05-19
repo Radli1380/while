e1cb4e30d98f8eb85ae8fa3aa00beeacad58e0a9
Radli1380 <ildarshakirow@mail.ru>
2024-05-19 15:52:46 +0300

    Add files via upload

---
+++ while.py
@@ -0,0 +1,10 @@
+#x>0
+#result -19
+#enter your name
+while 1 > 0:
+    number = int(input("Введите число: "))
+    if number > 19:
+        name = input("Введите ваше имя: ")
+        print("Привет,", name)
+    else:
+             print("Нет доступа")
